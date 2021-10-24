cabc6f0b55f87cb9d6af67dbd56956b0d50106bb
Rien Croonenborghs <rien@croonenborghs.net>
2021-10-25 08:39:41 +1300

    avatar and sunrise/sunset calculator

--- app/services/darkmode.rb
+++ app/services/darkmode.rb
@@ -1,8 +1,14 @@
+require "solareventcalculator.rb"
+
 class Darkmode
   def self.darkmode?
     now = Time.zone.now
-    seven_am = now.beginning_of_day + 7.hours
-    seven_pm = now.beginning_of_day + 19.hours
-    !now.between?(seven_am, seven_pm)
+    # Wellington
+    latitude = 41.2924
+    longitude = 174.7787
+    calc = SolarEventCalculator.new(Date.current, latitude, longitude)
+    sunrise = calc.compute_official_sunrise("Pacific/Auckland")
+    sunset = calc.compute_official_sunset("Pacific/Auckland")
+    !now.between?(sunrise, sunset)
   end
 end
