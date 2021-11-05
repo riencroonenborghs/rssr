c0d778e622c6302fde5fe2ba54f09d7c7f07ba03
Rien Croonenborghs <rien@croonenborghs.net>
2021-11-06 12:02:19 +1300

    fix migrations

--- db/migrate/20211101045403_create_subscriptions.rb
+++ db/migrate/20211101045403_create_subscriptions.rb
@@ -1,15 +1,15 @@
-class CreateUserFeeds < ActiveRecord::Migration[6.0]
+class CreateSubscriptions < ActiveRecord::Migration[6.0]
   def change
-    create_table :user_feeds do |t|
+    create_table :subscriptions do |t|
       t.references :user, null: false, foreign_key: true
       t.references :feed, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_index :user_feeds, [:user_id, :feed_id], unique: true
+    add_index :subscriptions, [:user_id, :feed_id], unique: true
 
     Feed.all.each do |feed|
-      UserFeed.create!(user_id: feed.user_id, feed_id: feed.id)
+      Subscription.create!(user_id: feed.user_id, feed_id: feed.id)
     end
 
     remove_column :feeds, :user_id
--- db/migrate/20211101073311_rename_user_feeds_to_subscriptions.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserFeedsToSubscriptions < ActiveRecord::Migration[6.0]
-  def change
-    rename_table :user_feeds, :subscriptions
-  end
-end
