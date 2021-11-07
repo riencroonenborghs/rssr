7281bc0b44974c8580c81b95ca6516ed0889d4f4
Rien Croonenborghs <rien@croonenborghs.net>
2021-11-07 21:33:55 +1300

    read later on mobile
    count
    
    count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,35 @@ class ApplicationController < ActionController::Base
   # before_action :darkmode?
   before_action :mobile?
 
+  before_action :set_today_count
+  before_action :set_read_later_count
+  before_action :set_subscription_count
+
+  def set_today_count
+    @today_count = filtered_scope do
+      scope = Entry
+              .includes(:viewed_by)
+              .most_recent_first
+              .today
+              .joins(feed: { subscriptions: :user })
+              .distinct
+      scope = scope.merge(User.where(id: current_user.id)) if user_signed_in?
+      scope
+    end.count
+  end
+
+  def set_read_later_count
+    @read_later_count = 0 and return unless user_signed_in?
+
+    @read_later_count = current_user.read_later_entries.count
+  end
+
+  def set_subscription_count
+    @subscription_count = 0 and return unless user_signed_in?
+
+    @subscription_count = current_user.subscriptions.count
+  end
+
   private
 
   def set_offset
