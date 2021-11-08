107fc380dc886799cc24f3fb850fce390223c422
Rien Croonenborghs <rien@croonenborghs.net>
2021-11-08 17:00:16 +1300

    fix read it

--- app/controllers/read_later_entries_controller.rb
+++ app/controllers/read_later_entries_controller.rb
@@ -2,7 +2,7 @@ class ReadLaterEntriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @entries = Entry.joins(feed: { subscriptions: :user }).joins(:read_later).merge(current_user.read_later_entries)
+    @entries = Entry.joins(feed: { subscriptions: :user }).joins(:read_later).merge(current_user.read_later_entries).distinct
   end
 
   def create
