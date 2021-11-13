0ef50c09f0abc1bad29a4bc1327ca28a2854b58a
Rien Croonenborghs <rien@croonenborghs.net>
2021-11-13 15:24:05 +1300

    yup

--- app/services/backfill/set_image_url_on_feeds.rb
+++ app/services/backfill/set_image_url_on_feeds.rb
@@ -1,12 +1,12 @@
 module Backfill
   class SetImageUrlOnFeeds < AppService
-    def call
+    def call # rubocop:disable Metrics/MethodLength
       Feed.where(image_url: nil).each do |feed|
         pp "feed #{feed.name} (#{feed.url})"
         feed.guess_image_url
         if feed.image_url_changed?
           pp "-- found url, saving"
-          feed.save 
+          feed.save
         end
       rescue StandardError => e
         pp "-- something's up"
