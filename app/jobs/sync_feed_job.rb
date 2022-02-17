a90660d7d3c1d453ae6eee4a0303262cae4a5775
Rien Croonenborghs <rien@croonenborghs.net>
2022-02-17 16:21:45 +1300

    active

--- app/jobs/sync_feed_job.rb
+++ app/jobs/sync_feed_job.rb
@@ -2,7 +2,8 @@ class SyncFeedJob < ApplicationJob
   queue_as :default
 
   def perform(feed_id)
-    feed = Feed.find(feed_id)
+    # only when the corresponding subscription is active
+    feed = Feed.joins(:subscriptions).where(id: feed_id).merge(Subscription.active)
     return unless feed
 
     feed.sync!
