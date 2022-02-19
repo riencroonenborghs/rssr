44eb6625b6005d77d31980773faffd35ee624d4b
Rien Croonenborghs <rien@croonenborghs.net>
2022-02-19 18:53:06 +1300

    fix remove old entries job

--- app/jobs/remove_old_entries_job.rb
+++ app/jobs/remove_old_entries_job.rb
@@ -4,7 +4,7 @@ class RemoveOldEntriesJob < ApplicationJob
   def perform
     Entry
       .where("entries.created_at <= ?",  2.weeks.ago)
-      .where.not(id: ReadLaterEntry.unread.select(:id))
+      .where.not(id: ReadLaterEntry.select(:id))
       .in_batches(of: 200)
       .destroy_all
   end
