27f75f55b93174bd748cce90e8dc3e5bae269e5c
Rien Croonenborghs <rien@croonenborghs.net>
2022-05-01 11:16:57 +1200

    fix remove old entries job

--- app/sidekiq/remove_old_entries_job.rb
+++ app/sidekiq/remove_old_entries_job.rb
@@ -4,7 +4,7 @@ class RemoveOldEntriesJob
   def perform
     Entry
       .where("entries.created_at <= ?", 2.weeks.ago)
-      .where.not(id: Bookmark.select(:id))
+      .where.not(id: Bookmark.select(:entry_id))
       .in_batches(of: 200)
       .destroy_all
   end
