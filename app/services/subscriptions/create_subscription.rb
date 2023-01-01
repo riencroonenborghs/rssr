dc1dde34ab71aee6082e4a9d977e84b0deebaae7
Rien Croonenborghs <rien@croonenborghs.net>
2023-01-02 09:24:17 +1300

    create sub
    find or create the feed
    and creates new sub
    errors when it fails
    guesses the name either from the rss feed
    or the url provided

--- app/services/subscriptions/create_subscription.rb
+++ app/services/subscriptions/create_subscription.rb
@@ -4,15 +4,24 @@ module Subscriptions
   class CreateSubscription
     include Base
 
-    attr_reader :user, :name, :tag_list, :url, :description,
-                :feed, :subscription
+    attr_reader :feed, :subscription
+    attr_reader :default_feed, :default_subscription
 
-    def initialize(user:, name:, tag_list:, url:, description:)
+    def initialize(user:, name:, tag_list:, url:, description:, hide_from_main_page:)
       @user = user
       @name = name
       @tag_list = tag_list
       @url = url
       @description = description
+      @hide_from_main_page = hide_from_main_page
+
+      @default_feed = Feed.new(
+        name: name,
+        url: url,
+        tag_list: tag_list,
+        description: description
+      )
+      @default_subscription = user.subscriptions.build(feed: default_feed, hide_from_main_page: hide_from_main_page)
     end
 
     def perform
@@ -23,7 +32,7 @@ module Subscriptions
         subscription_exists?
         return unless success?
 
-        @subscription = user.subscriptions.build(feed_id: feed.id)
+        @subscription = user.subscriptions.build(feed_id: feed.id, hide_from_main_page: hide_from_main_page)
 
         if subscription.save
           RefreshFeedJob.perform_in(5.seconds, @feed.id)
@@ -35,11 +44,36 @@ module Subscriptions
 
     private
 
+    attr_reader :user, :name, :tag_list, :url, :description, :hide_from_main_page
+    attr_reader :rss_feeds, :guesser
+
     def find_or_create_feed
       return if (@feed = Feed.find_by(url: url))
+      
+      create_feed
+    end
 
-      build_feed
+    def create_feed
+      @feed = Feed.new(name: name, url: url, tag_list: tag_list, description: description)
+
+      find_rss_feeds
+      return unless success?
+
+      feed.rss_url = rss_feeds.first.href
+      feed.name = rss_feeds.first.title if feed.name.blank?
+
+      guess_details
+      feed.name = guesser.name if guesser.success? && feed.name.blank?
+
       errors.merge!(feed.errors) unless feed.save
+    end
+
+    def find_rss_feeds
+      finder = FindRssFeeds.perform(url: url)
+      errors.merge!(finder.errors) unless finder.success?
+      errors.add(:base, "no RSS feeds found") and return unless finder.rss_feeds.any?
+
+      @rss_feeds = finder.rss_feeds
     end
 
     def subscription_exists?
@@ -48,17 +82,8 @@ module Subscriptions
       errors.add(:base, "subscription already exists")
     end
 
-    def build_feed
-      @feed = Feed.new(name: name, url: url, tag_list: tag_list, description: description)
-      feed.feed_type =  case url
-                        when /reddit\.com/
-                          Feed::SUBREDDIT
-                        else
-                          Feed::RSS
-                        end
-
-      loader = Feeds::GuessDetails.perform(feed: feed)
-      feed.name = loader.name if loader.success?
+    def guess_details
+      @guesser = Feeds::GuessDetails.perform(feed: feed)
     end
   end
 end
--- spec/services/subscriptions/create_subscription_spec.rb
+++ spec/services/subscriptions/create_subscription_spec.rb
@@ -4,45 +4,126 @@ Sidekiq::Testing.fake!
 
 module Subscriptions
   RSpec.describe CreateSubscription, type: :service do
-    subject { described_class.new(user: user, name: name, tag_list: tag_list, url: url, description: description) }
+    subject { described_class.perform(user: user, name: name, tag_list: tag_list, url: url, description: description, hide_from_main_page: hide_from_main_page) }
 
     let(:user) { create :user }
     let(:name) { "Name" }
     let(:tag_list) { "foo,bar,baz" }
     let(:url) { Faker::Internet.url }
     let(:description) { "description" }
+    let(:hide_from_main_page) { false }
 
-    let(:mock_guess_success) { true }
-    let(:mock_guess_name) { "name" }
-    let(:mock_guess_errors) { nil }
-    let(:mock_guess) { instance_double(Feeds::GuessDetails, success?: mock_guess_success, name: mock_guess_name, errors: mock_guess_errors) }
+    let(:finder_success) { true }
+    let(:rss_feed) { FindRssFeeds::RssFeed.new(href: Faker::Internet.url) }
+    let(:finder_rss_feeds) { [rss_feed] }
+    let(:finder_errors) { nil }
+    let(:mock_finder) { instance_double(FindRssFeeds, success?: finder_success, rss_feeds: finder_rss_feeds, errors: finder_errors) }
+
+    let(:guesser_success) { true }
+    let(:guesser_name) { "name" }
+    let(:guesser_errors) { nil }
+    let(:mock_guesser) { instance_double(Feeds::GuessDetails, success?: guesser_success, name: guesser_name, errors: guesser_errors) }
 
     before do
-      allow(Feeds::GuessDetails).to receive(:perform).and_return(mock_guess)
+      allow(FindRssFeeds).to receive(:perform).and_return(mock_finder)
+      allow(Feeds::GuessDetails).to receive(:perform).and_return(mock_guesser)
     end
 
     describe "#perform" do
       context "when the feed is new" do
         context "when saving the feed fails" do
+          let(:error_message) { "some feed error" }
+
           before do
             errors = ActiveModel::Errors.new(Feed)
-            errors.add(:base, "some feed error")
+            errors.add(:base, error_message)
             feed = build :feed
             allow(feed).to receive(:save).and_return false
             allow(feed).to receive(:errors).and_return errors
             allow(Feed).to receive(:new).and_return(feed)
           end
 
-          it_behaves_like "the service fails with error", "some feed error"
+          it_behaves_like "the service fails"
+        end
+
+        it "finds the rss feed" do
+          expect(FindRssFeeds).to receive(:perform)
+          subject
+        end
+
+        context "when finding the RSS feeds fails" do
+          let(:error_message) { "Some error" }
+          let(:finder_success) { false }
+          let(:finder_errors) do
+            errors = ActiveModel::Errors.new(FindRssFeeds)
+            errors.add(:base, error_message)
+            errors
+          end
+
+          it_behaves_like "the service fails"
+        end
+
+        context "when there's no RSS feeds" do
+          let(:error_message) { "no RSS feeds found" }
+          let(:finder_rss_feeds) { [] }
+
+          it_behaves_like "the service fails"
         end
 
         it "creates the feed" do
-          expect { subject.perform }.to change(Feed, :count).by(1)
+          expect { subject }.to change(Feed, :count).by(1)
         end
 
-        it "guesses the name" do
-          expect(Feeds::GuessDetails).to receive(:perform)
-          subject.perform
+        it "sets the rss url" do
+          subject
+          feed = Feed.last
+          expect(feed.rss_url).to eq finder_rss_feeds.first.href
+        end
+
+        it "sets the name" do
+          subject
+          feed = Feed.last
+          expect(feed.name).to eq name
+        end
+
+        context "when no name was given" do
+          let(:name) { nil }
+
+          context "when rss feed finder has the name" do
+            let(:rss_feed_name) { "name" }
+            let(:rss_feed) { FindRssFeeds::RssFeed.new(href: Faker::Internet.url, title: rss_feed_name) }
+
+            it "sets the rss finder name" do
+              subject
+              feed = Feed.last
+              expect(feed.name).to eq rss_feed_name
+            end
+          end
+
+          it "guesses the name" do
+            expect(Feeds::GuessDetails).to receive(:perform)
+            subject
+          end
+
+          it "sets the guesser name" do
+            subject
+            feed = Feed.last
+            expect(feed.name).to eq guesser_name
+          end
+
+          context "when guessing the name fails" do
+            let(:error_message) { "Some error" }
+            let(:guesser_success) { false }
+            let(:guesser_errors) do
+              errors = ActiveModel::Errors.new(Feeds::GuessDetails)
+              errors.add(:base, error_message)
+              errors
+            end
+  
+            it "leaves the name blank" do
+              expect(subject.feed.name).to be_nil
+            end
+          end
         end
       end
 
@@ -50,33 +131,36 @@ module Subscriptions
         let!(:feed) { create :feed, url: url }
 
         it "does not creates the feed" do
-          expect { subject.perform }.to_not change(Feed, :count)
+          expect { subject }.to_not change(Feed, :count)
         end
       end
 
       context "when the subscription is new" do
         context "when saving the subscription fails" do
+          let(:error_message) { "some subscription error" }
+
           before do
             object = User.new
-            object.errors.add(:base, "some subscription error")
+            object.errors.add(:base, error_message)
 
             subscription = instance_double(Subscription, save: false, errors: object.errors)
             allow(user.subscriptions).to receive(:build).and_return(subscription)
           end
 
-          it_behaves_like "the service fails with error", "some subscription error"
+          it_behaves_like "the service fails"
         end
 
         it "creates the subscription" do
-          expect { subject.perform }.to change(Subscription, :count).by(1)
+          expect { subject }.to change(Subscription, :count).by(1)
         end
       end
 
       context "when the subscription exists" do
+        let(:error_message) { "subscription already exists" }
         let!(:feed) { create :feed, url: url }
         let!(:subscription) { user.subscriptions.create!(feed: feed) }
 
-        it_behaves_like "the service fails with error", "subscription already exists"
+        it_behaves_like "the service fails"
       end
     end
 
@@ -84,7 +168,7 @@ module Subscriptions
       feed = create :feed, url: url
       travel_to Time.zone.now do
         expect(RefreshFeedJob).to receive(:perform_in).with(5.seconds, feed.id)
-        subject.perform
+        subject
       end
     end
   end
