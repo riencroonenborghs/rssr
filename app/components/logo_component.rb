5d8c603214bd670239548e79adffc10f74d0a1ff
Rien Croonenborghs <rien@croonenborghs.net>
2023-10-15 12:16:47 +1300

    subs

---
+++ app/components/logo_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class LogoComponent < ViewComponent::Base
+  def initialize(feed:, mark_viewed: false) # rubocop:disable Lint/MissingSuper
+    @name = feed.name
+    @name = @name.split(/ /).first(2).map { |x| x.first }.compact.join
+    @mark_viewed = mark_viewed
+  end
+end
