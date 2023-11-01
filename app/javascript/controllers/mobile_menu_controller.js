0de1502d1f4405c9c67ad99d9604b682fe72a862
Rien Croonenborghs <rien@croonenborghs.net>
2023-11-01 20:56:40 +1300

    new mobile sidebar

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -2,14 +2,28 @@ import { Controller, del } from "@hotwired/stimulus"
 
 export default class extends Controller {
   showClicked (event) {
-    document.querySelector('#menu').classList.remove('hidden')
+    let menu = document.querySelector('#menu')
+    menu.classList.remove('hidden')
+    let parent = menu.parentNode
+    parent.classList.add('bg-opacity-50')
+    parent.classList.add('bg-gray-900')
+    let nav = parent.querySelector('nav')
+    nav.classList.add('bg-opacity-10')
+    nav.classList.add('bg-gray-900')
 
     event.preventDefault()
     event.stopPropagation()
   }
 
   hideClicked (event) {
-    document.querySelector('#menu').classList.add('hidden')
+    let menu = document.querySelector('#menu')
+    menu.classList.add('hidden')
+    let parent = menu.parentNode
+    parent.classList.remove('bg-opacity-50')
+    parent.classList.remove('bg-gray-900')
+    let nav = parent.querySelector('nav')
+    nav.classList.remove('bg-opacity-10')
+    nav.classList.remove('bg-gray-900')
 
     event.preventDefault()
     event.stopPropagation()
