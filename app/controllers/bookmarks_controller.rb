2fcba20065c6968f9a7203db3a4f5f49b2e13bbe
Rien Croonenborghs <rien@croonenborghs.net>
2023-11-11 13:56:30 +1300

    bookmark ordering

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,11 +4,16 @@ class BookmarksController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    entry_ids = current_user.bookmarks.order(created_at: :desc).select(:entry_id).pluck(:entry_id)
+
     @entries = Entry
       .includes(feed: { subscriptions: { taggings: :tag } })
-      .where(id: current_user.bookmarks.select(:entry_id))
-      .order(current_user.bookmarks.order(created_at: :desc).pluck(:entry_id).map { |id| "id = #{id} DESC" })
+      .where(id: entry_ids)
       .page(page).per(@pagination_size)
+
+    @entries = @entries.sort_by do |entry|
+      entry_ids.index(entry.id)
+    end
 
     return paged_render if params.key?(:page) # rubocop:disable Style/RedundantReturn
   end
