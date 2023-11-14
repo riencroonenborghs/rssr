83cfa6a3226c774cc5f8d0a118acd344ccc29546
Rien Croonenborghs <rien@croonenborghs.net>
2023-11-14 19:27:38 +1300

    bookmarks fix

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,10 +4,10 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV.fetch("ROLLBAR_ACCESS_TOKEN")
+  config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
 
-  # Here we'll disable in 'test':
-  if Rails.env.test?
+  # Here we'll disable when not in prod:
+  unless Rails.env.production?
     config.enabled = false
   end
 
