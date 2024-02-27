a5eb280722c5e3af344f3a625a7a726aeec3ac9b
Rien Croonenborghs <rien@croonenborghs.net>
2024-02-28 08:04:57 +1300

    random refresh all feeds

--- app/sidekiq/refresh_subscriptions_job.rb
+++ app/sidekiq/refresh_subscriptions_job.rb
@@ -5,7 +5,8 @@ class RefreshSubscriptionsJob
 
   def perform
     Subscription.active.distinct(:feed_id).pluck(:feed_id).each do |feed_id|
-      RefreshFeedJob.perform_async({ feed_id: feed_id }.to_json)
+      wait_time_in_s = rand(1..90)
+      RefreshFeedJob.perform_in(wait_time_in_s.seconds, { feed_id: feed_id }.to_json)
     end
   end
 end
