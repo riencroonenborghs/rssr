e27548b16fda715a52eb9459f8e48f96295c7cb1
Rien Croonenborghs <rien@croonenborghs.net>
2024-10-15 06:02:06 +1100

    notiifcations

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,10 +4,10 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def ack
-    notification = current_user.notifications.where(id: params[:id])
+    notification = current_user.notifications.where(id: params[:id]).first
     return unless notification
   
-    notification.update(acked_at: Time.zone.now)
+    current_user.notifications.where(watch_group_id: notification.watch_group_id).update(acked_at: Time.zone.now)
     render plain: "acked"
   end
 end
