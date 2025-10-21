006bc208007ed5b3c3a2817c9b47743933567883
Rien <rien@croonenborghs.net>
2025-10-21 20:12:21 +1100

    fix mobile viewed

--- app/javascript/controllers/mobile_mark_as_read_controller.js
+++ app/javascript/controllers/mobile_mark_as_read_controller.js
@@ -3,26 +3,13 @@ import { Controller, del } from "@hotwired/stimulus"
 export default class extends Controller {
   markAsRead (event) {
     const entry = event.target.closest('.entry')
-    const entryId = entry.dataset.entryId
     const logo = entry.querySelector('.entry--logo .entry--logo--logo')
+    entry.classList.add('bg-zinc-100')
+    entry.classList.add('text-zinc-400')
 
-    let headers = {}
-    headers['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').content
-
-    fetch(
-      `/viewed/${entryId}`,
-      {
-        method: 'POST',
-        headers: headers
-      }
-    ).then((result) => {
-      if (result.status === 200) {
-        entry.classList.add('bg-zinc-100')
-        entry.classList.add('text-zinc-400')
-        // entry.classList.add('rounded-lg')
-        logo.classList.remove('bg-zinc-500')
-        logo.classList.add('bg-zinc-400')
-      }
-    })
+    if(logo !== null) {
+      logo.classList.remove('bg-zinc-500')
+      logo.classList.add('bg-zinc-400')
+    }
   }
 }
