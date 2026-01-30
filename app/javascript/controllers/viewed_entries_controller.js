43cc1709f396c7e9fd11a25747a50856390eb316
Rien <rien@croonenborghs.net>
2026-01-31 08:20:08 +1100

    viewed fix

--- app/javascript/controllers/viewed_entries_controller.js
+++ app/javascript/controllers/viewed_entries_controller.js
@@ -2,7 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   markAsViewed (event) {
-    const entry = event.target.closest('.entry')
-    entry.classList.add('bg-gray-50')
+    const entry = event.target.closest('.entry .title')
+    console.log(entry)
+    entry.classList.add('line-through')
   }
 }
